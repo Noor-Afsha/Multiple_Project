c91eccce74fe0da68c71c220b26c3ecebf34f7ca
Noor Afsha Khatoon <noor.afsha@wecodee.com>
2024-09-11 16:20:56 +0530

    date application

---
+++ src/main/java/com/Wecoodee/CustomerAppliaction/DateProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.Wecoodee.CustomerAppliaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateProjectsApplication.class, args);
+	}
+
+}
