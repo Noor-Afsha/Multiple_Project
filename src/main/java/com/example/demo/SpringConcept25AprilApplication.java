912ee687c415e67f308b988c6f67dda8a51fdbc0
Noor Afsha Khatoon <noor.afsha@wecodee.com>
2024-09-11 17:19:14 +0530

    spring concept

---
+++ src/main/java/com/example/demo/SpringConcept25AprilApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConcept25AprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConcept25AprilApplication.class, args);
+	}
+
+}
